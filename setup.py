f3a396ee7aadbf03900abda2972f5b5211ea81c6
Johannes Habel <EchterAlsFake@proton.me>
2025-01-05 19:44:31 +0100

    - separate package name for async lib

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="eaf_base_api",
+    name="eaf_base_api-async",
     version="2.1.1",
     packages=find_packages(),
     install_requires=["ffmpeg-progress-yield", "m3u8", "httpx"],
