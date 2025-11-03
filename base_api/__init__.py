81eba51a2f88c2c878f9dceb57a50448171ac4f7
Johannes Habel <EchterAlsFake@proton.me>
2025-11-03 17:43:39 +0100

    - added cache for segments - updated __init__ for imports

--- base_api/__init__.py
+++ base_api/__init__.py
@@ -1,5 +1,7 @@
-__all__ = ["BaseCore", "Callback", "config"]
+__all__ = ["BaseCore", "Callback", "config", "errors", "setup_logger", "Cache"]
 
-from base_api.base import BaseCore
-from base_api.modules.progress_bars import Callback
+
+from base_api.modules import errors
 from base_api.modules.config import config
+from base_api.modules.progress_bars import Callback
+from base_api.base import BaseCore, setup_logger, Cache
--- base_api/modules/config.py
+++ base_api/modules/config.py
@@ -7,7 +7,6 @@ class RuntimeConfig:
         self.max_bandwidth_mb = None # Set speed limit in megabytes per second e.g, 2.0, 3.5 etc...
         self.ffmpeg_path = "ffmpeg"
         self.proxy = None
-        self.raise_bot_protection = False
         self.verify_ssl = True
         self.cookies = None
         self.locale = "en-US,en;q=0.9" # If you override this, it could change regexes and thus make stuff not work...
